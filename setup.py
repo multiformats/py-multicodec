23213b8b40b21e17e2e1844224498cbd8e359bfa
Dhruv Baldawa <dhruvbaldawa@gmail.com>
2018-10-20 08:50:24 +0400

    Bump version: 0.1.2 → 0.1.3

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ test_requirements = [
 
 setup(
     name='py-multicodec',
-    version='0.1.2',
+    version='0.1.3',
     description="Multicodec implementation in Python",
     long_description=readme + '\n\n' + history,
     author="Dhruv Baldawa",
