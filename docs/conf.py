e53133e88a2139a1b18b5de0ceec4765d6dc1928
pacrob <5199899+pacrob@users.noreply.github.com>
2025-12-18 10:21:53 -0700

    add doctest to sphinx conf

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,7 @@ import multicodec
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.doctest']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
